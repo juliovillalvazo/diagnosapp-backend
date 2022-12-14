4ada83af3180ddd7e1a08770e7f8c7c584fc21b0
Julio Villalvazo Carrera <juliovillalvazo26@gmail.com>
2022-12-13 21:02:21 -0600

    added feedback endpoint

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,29 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+router.get('/', (req, res, next) => {
+    res.json('All good in here');
+});
+
+router.post('/send-feedback', async (req, res, next) => {
+    try {
+        const { from_name, message } = req.body;
+        const data = {
+            service_id: process.env.SERVICE_ID,
+            template_id: process.env.TEMPLATE_ID,
+            user_id: process.env.USER_ID,
+            accessToken: process.env.accessToken,
+            template_params: {
+                from_name: from_name,
+                message: message,
+            },
+        };
+        await axios.post('https://api.emailjs.com/api/v1.0/email/send', data);
+        res.send(201);
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
